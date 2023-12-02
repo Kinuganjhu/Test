78917195cee74c6dd6a056def7229f8552bb6e8b
Kinuganjhu <136769553+Kinuganjhu@users.noreply.github.com>
2023-12-02 23:44:14 +0000

    Add O button

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
-export default function Board(value) {
+export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setXIsNext] = useState(false); // Adjusted initial value to true
 
   const handleSquareClick = (clickedPosition) => {
+    if (squares[clickedPosition]) {
+      return;
+    }
+    
     setSquares((currentSquares) => {
       return currentSquares.map((squareValue, position) => {
         if (clickedPosition === position) {
-          return 'X';
+          return isXNext ? 'X' : 'O'; // Changed '0' to 'O' for 'O' symbol
         }
         return squareValue;
       });
     });
+    setXIsNext((currentIsNext) => !currentIsNext); // Fixed syntax for setting isXNext state
   };
 
   const renderSquare = (position) => {
