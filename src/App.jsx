f46bd8e1e9682d25e260fbb73678cd915e28a1a3
Kinuganjhu <136769553+Kinuganjhu@users.noreply.github.com>
2023-12-02 10:24:55 +0000

    Basic complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
-import Home  from './Pages/Home'; // Assuming Home is a default export
 
+import './styles.scss'; 
+import Board from './Components/Board'; 
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Home />
+      <h1>TIC TAC TOE GAME</h1>
+<Board/>
+      
     </div>
   );
 }
