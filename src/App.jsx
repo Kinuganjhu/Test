c29b748e3189fb44aff24ffc1767efee49411a5a
Kinuganjhu <136769553+Kinuganjhu@users.noreply.github.com>
2023-12-03 00:45:17 +0000

    Add winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import React, { useState } from 'react';
+import './styles.scss';
+import Board from './Components/Board';
+import calculateWinner from './Components/winner';
 
-import './styles.scss'; 
-import Board from './Components/Board'; 
 export default function App() {
-  return (
-    <div className = 'App'>
-      <h1>TIC TAC TOE GAME</h1>
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true); // Changed initial value to true
+  const winner = calculateWinner(squares); // Renamed 'Winner' to 'winner' for consistency
   
-<Board/>
-      
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner ? `Winner is ${winner}` : `Next Player is ${nextPlayer}`;
+
+  const handleSquareClick = (clickedPosition) => {
+    if (squares[clickedPosition] || winner) {
+      return; // Stop processing if there's a winner or square is already filled
+    }
+
+    setSquares((currentSquares) => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setIsXNext((prevIsNext) => !prevIsNext); // Changed setXIsNext to setIsXNext for clarity
+  };
+
+  return (
+    <div className='App'>
+      <h1>{statusMessage}</h1>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setXIsNext] = useState(false); // Adjusted initial value to true
-
-  const handleSquareClick = (clickedPosition) => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-    
-    setSquares((currentSquares) => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : 'O'; // Changed '0' to 'O' for 'O' symbol
-        }
-        return squareValue;
-      });
-    });
-    setXIsNext((currentIsNext) => !currentIsNext); // Fixed syntax for setting isXNext state
-  };
-
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = (position) => {
     return <Square value={squares[position]} onClick={() => handleSquareClick(position)} />;
   };
@@ -51,4 +32,6 @@ export default function Board() {
       </div>
     </>
   );
-}
+};
+
+export default Board;
